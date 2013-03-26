3d76736e7f33bbf158cca7a1c34fc615a83618a2
Tony Schneider <tonywok@gmail.com>
2013-03-26 16:52:00 -0400

    prototype comments

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ App.Router.map(function() {
   this.resource("parts", function() {
     this.route("new");
     this.resource("part", { path: "/:part_id" }, function() {
-      this.resource("version", { path: "/:version_id" });
+      this.resource("version", { path: "/versions/:version_id" });
     });
   });
 });
@@ -91,12 +91,60 @@ App.PartsNewController = Ember.ObjectController.extend({
   }
 });
 
+App.PartController = Ember.ObjectController.extend({
+  isSaved: true
+});
+
+App.VersionController = Ember.ObjectController.extend({
+  isSaved: true,
+
+  revise: function() {
+    this.set('isSaved', true);
+  },
+
+  saveState: function() {
+    if (this.get("isSaved")) {
+      return "Saved"
+    } else {
+      return "Save"
+    }
+  }.property("isSaved")
+
+});
+
+//Views
+//
+App.DjpTextArea = Ember.TextArea.extend({
+  didInsertElement: function() {
+    this.set("original_content", this.get("value"));
+  },
+
+  change: function() {
+    if (this.get('original_content') === this.get('value')) {
+      this.set("controller.isSaved", true);
+    } else {
+      this.set("controller.isSaved", false);
+    }
+  }.observes("value")
+});
+
 //Models
 //
+App.Comment = Ember.Object.extend({
+});
+
 App.Version = Ember.Object.extend({
-  id: function() {
+  name: function() {
     return this.get("part.name");
   }.property("part.name"),
+
+  comments: function() {
+    return [
+      App.Comment.create({id: "1", body: "That song totally rocks!", part: this}),
+      App.Comment.create({id: "2", body: "Could use a bit more base, no?", part: this}),
+      App.Comment.create({id: "3", body: "Turn it up to 11!", part: this})
+    ];
+  }.property(),
 });
 
 App.Part = Ember.Object.extend({
@@ -104,9 +152,19 @@ App.Part = Ember.Object.extend({
     return this.get("name");
   }.property("name"),
 
-  versions: [
-    App.Version.create({id: "1", body: "bar"}),
-    App.Version.create({id: "2", body: "bar"}),
-    App.Version.create({id: "3", body: "bar"})
-  ]
+  comments: function() {
+    return [
+      App.Comment.create({id: "1", body: "That song totally rocks!", part: this}),
+      App.Comment.create({id: "2", body: "Could use a bit more base, no?", part: this}),
+      App.Comment.create({id: "3", body: "Turn it up to 11!", part: this})
+    ];
+  }.property(),
+
+  versions: function() {
+    return [
+      App.Version.create({id: "1", body: "bar", part: this}),
+      App.Version.create({id: "2", body: "bar", part: this}),
+      App.Version.create({id: "3", body: "bar", part: this})
+    ];
+  }.property()
 });
