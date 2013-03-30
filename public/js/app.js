e11c00bbfc75fbb6d4fff064aa96a91986908cad
Tony Schneider <tonywok@gmail.com>
2013-03-29 23:25:08 -0400

    inline notes

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+
 window.App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
@@ -24,6 +25,11 @@ App.IndexRoute = Ember.Route.extend({
 App.PartsRoute = Ember.Route.extend({
   model: function() {
     return [App.Part.create({name: "Verse 1", body: "lorem" })];
+  },
+  setupController: function(controller) {
+    this.controllerFor("part_comments.new").set("content", App.Comment.create({
+      body: "",
+    }));
   }
 });
 
@@ -31,7 +37,7 @@ App.PartsRoute = Ember.Route.extend({
     redirect: function() {
       this.transitionTo('parts.new');
     }
-  })
+  });
 
   App.PartsNewRoute = Ember.Route.extend({
     model: function() {
@@ -49,7 +55,6 @@ App.PartRoute = Ember.Route.extend({
     var part = this.controllerFor('parts').get('content').find(function(part) {
       return part.get("name") === params.part_id;
     });
-
     if (!part) this.transitionTo("parts.new");
     return part;
   }
@@ -66,16 +71,19 @@ App.PartRoute = Ember.Route.extend({
 App.VersionRoute = Ember.Route.extend({
   model: function(params) {
     var part = this.modelFor("part");
-
     var version = part.get('versions').find(function(v) {
       return v.get("id") === params.version_id;
     });
     if (!version) this.transitionTo("part", part);
     return version;
   },
-
   renderTemplate: function() {
     this.render("version", { outlet: "parts", into: "parts" });
+  },
+  setupController: function(controller) {
+    this.controllerFor("version_comments.new").set("content", App.Comment.create({
+      body: "",
+    }));
   }
 });
 
@@ -83,7 +91,6 @@ App.VersionRoute = Ember.Route.extend({
 //
 App.PartsNewController = Ember.ObjectController.extend({
   needs: ["parts"],
-
   create: function() {
     var part = this.get("content");
     this.get("controllers.parts").pushObject(part);
@@ -92,24 +99,58 @@ App.PartsNewController = Ember.ObjectController.extend({
 });
 
 App.PartController = Ember.ObjectController.extend({
-  isSaved: true
+  isSaved: true,
 });
 
 App.VersionController = Ember.ObjectController.extend({
   isSaved: true,
-
   revise: function() {
     this.set('isSaved', true);
   },
-
   saveState: function() {
-    if (this.get("isSaved")) {
-      return "Saved"
-    } else {
-      return "Save"
-    }
-  }.property("isSaved")
+    return ((this.get("isSaved")) ? "Saved" : "Save");
+  }.property("isSaved"),
+});
 
+App.PartCommentsNewController = Ember.ObjectController.extend({
+  needs: ["part"],
+  post: function(comment) {
+    var partController = this.get("controllers.part");
+    var part = partController.get("content");
+    part.get("comments").pushObject(comment);
+    this.set("content", App.Comment.create({ body: "" }));
+  }
+});
+
+App.VersionCommentsNewController = Ember.ObjectController.extend({
+  needs: ["version"],
+  post: function(comment) {
+    var versionController = this.get("controllers.version");
+    var version = versionController.get("content");
+    version.get("comments").pushObject(comment);
+    this.set("content", App.Comment.create({ body: "" }));
+  }
+});
+
+App.LineController = Ember.ObjectController.extend({
+  showingNewNote: false,
+
+  showingNotes: function() {
+   return this.get("showingNewNote") || this.get("foos").length;
+  }.property("foos", "showingNewNote"),
+
+  toggleNewNote: function() {
+    this.set("showingNewNote", !this.get("showingNewNote"));
+  },
+
+  createNote: function(note){
+    this.get("content.foos").pushObject(App.Comment.create({
+      body: note
+    }))
+  }
+});
+
+App.NoteController = Ember.ObjectController.extend({
 });
 
 //Views
@@ -118,7 +159,6 @@ App.DjpTextArea = Ember.TextArea.extend({
   didInsertElement: function() {
     this.set("original_content", this.get("value"));
   },
-
   change: function() {
     if (this.get('original_content') === this.get('value')) {
       this.set("controller.isSaved", true);
@@ -128,10 +168,13 @@ App.DjpTextArea = Ember.TextArea.extend({
   }.observes("value")
 });
 
+App.NoteView = Ember.View.extend({
+});
+
 //Models
 //
-App.Comment = Ember.Object.extend({
-});
+App.Comment = Ember.Object.extend({});
+App.Line = Ember.Object.extend({});
 
 App.Version = Ember.Object.extend({
   name: function() {
@@ -140,9 +183,9 @@ App.Version = Ember.Object.extend({
 
   comments: function() {
     return [
-      App.Comment.create({id: "1", body: "That song totally rocks!", part: this}),
-      App.Comment.create({id: "2", body: "Could use a bit more base, no?", part: this}),
-      App.Comment.create({id: "3", body: "Turn it up to 11!", part: this})
+      App.Comment.create({id: "1", body: "That song totally rocks!"}),
+      App.Comment.create({id: "2", body: "Could use a bit more base, no?"}),
+      App.Comment.create({id: "3", body: "Turn it up to 11!"})
     ];
   }.property(),
 });
@@ -154,17 +197,32 @@ App.Part = Ember.Object.extend({
 
   comments: function() {
     return [
-      App.Comment.create({id: "1", body: "That song totally rocks!", part: this}),
-      App.Comment.create({id: "2", body: "Could use a bit more base, no?", part: this}),
-      App.Comment.create({id: "3", body: "Turn it up to 11!", part: this})
+      App.Comment.create({id: "1", body: "That song totally rocks!"}),
+      App.Comment.create({id: "2", body: "Could use a bit more base, no?"}),
+      App.Comment.create({id: "3", body: "Turn it up to 11!"})
     ];
   }.property(),
 
   versions: function() {
     return [
-      App.Version.create({id: "1", body: "bar", part: this}),
-      App.Version.create({id: "2", body: "bar", part: this}),
-      App.Version.create({id: "3", body: "bar", part: this})
+      App.Version.create({
+        id: "1",
+        lines: [
+          App.Line.create({ 
+            id: "100",
+            body: "Oh, if you want my eyes take my eyes, they're always true",
+            foos: [
+              App.Comment.create({id: "10", body: "test"}),
+              App.Comment.create({id: "11", body: "test"})
+            ]
+          }),
+          App.Line.create({ id: "1", body: "Oh, If you want my heart take my heart, it's right here for you", foos: [] }),
+          App.Line.create({ id: "2", body: "Oh, it's been so long, but i made it through", foos: [] }),
+          App.Line.create({ id: "3", body: "Oh it's been so long, been so long, but i made it through", foos: [] }),
+          App.Line.create({ id: "4", body: "It's been so... long", foos: []})
+        ],
+        part: this
+      })
     ];
   }.property()
 });
